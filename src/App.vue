5890c127deb5400c613983db666dfb7e43639b3e
Inna Mon <inna.mon@gmail.com>
2019-11-16 17:01:16 -0600

    installed axios http package and sent requests to both get jsonp placeholder todo items, update them, and delete them

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
 import Header from './components/layout/Header.vue';
 import AddTodo from './components/AddTodo.vue';
 import TodoList from './components/TodoList.vue';
+import axios from 'axios';
 
 export default { 
   name: 'app',
@@ -25,35 +26,55 @@ export default {
   },
   methods: {
     deleteTodo(id) {
-      fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
-        method: 'DELETE'
-        // body: JSON.stringify({id: '5bdcdfa40f0a326f858feae0'})
-      })
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
       .then(response => {
         this.todoList = this.todoList.filter(todo => todo.id !== id)
         console.log('DELETE reponse', response);
       })
-      .catch(err => console.log(err))
+      .catch(err => console.log('DELETE response error', err))
+
+      // fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+      //   method: 'DELETE'
+      // })
+      // .then(response => {
+      //   this.todoList = this.todoList.filter(todo => todo.id !== id)
+      //   console.log('DELETE reponse', response);
+      // })
+      // .catch(err => console.log(err))
     },
     addTodo(newTodo) {
       const { title, completed } = newTodo;
-      console.log('checking newData', title, completed);
-      return fetch('https://jsonplaceholder.typicode.com/todos', {
-        method: 'POST',
-        body: JSON.stringify(title, completed)
+
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
       })
-      .then(response => response.json())
       .then(response => {
-        // this.todoList = [...this.todoList, response.data];
-        console.log('POST response', response);
+        this.todoList = [...this.todoList, response.data];
+        console.log('POST response', response.data);
       })
-      .catch(err => console.log(err))
+      .catch(err => console.log('POST request error', err)) 
     }
+
+    //   fetch('https://jsonplaceholder.typicode.com/todos', {
+    //     method: 'POST',
+    //     body: JSON.stringify(title, completed)
+    //   })
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     this.todoList = [...this.todoList, data];
+    //     console.log('POST response', data);
+    //   })
+    //   .catch(err => console.log('POST request error', err))
   },
   created() {
-    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
-    .then(response => response.json())
-    .then(data => this.todoList = data);
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    .then(response => this.todoList = response.data)
+    .catch(err => console.log('GET req error', err));
+
+    // fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    // .then(response => response.json())
+    // .then(data => this.todoList = data);
   }
 }
 </script>
