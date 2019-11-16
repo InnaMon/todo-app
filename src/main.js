2665e20a439d54a4fe4e0be69fed5c6b876b9ed6
Inna Mon <inna.mon@gmail.com>
2019-11-16 17:16:21 -0600

    npm installed vue router package

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router';
 import App from './App.vue'
+// import ourRoutes from './our-routes.js';
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter);
+
+// We create the router instance here.
+// const router = new VueRouter({
+//   routes: ourRoutes
+// });
 
 new Vue({
   render: h => h(App),
